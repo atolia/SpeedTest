74e43f5f51f8a3f1652973258622696c6cc412fc
root <atolia@gmail.com>
2016-12-13 14:40:22 +0300

    time in script

--- sql/convert-text-to-mysql.sh
+++ sql/convert-text-to-mysql.sh
@@ -3,4 +3,4 @@ mysqladmin -uroot create test 2>/dev/null
 
 awk -F \| 'BEGIN {print "drop table if exists `words`; create table words(id INTEGER PRIMARY KEY AUTO_INCREMENT, word varchar(10)); begin;"}
     {if(length($2)<=9  && length($2)>2)print "insert into words(word) values(\""$2"\");"}
-    END{print "commit;"}' words.txt |mysql test
+    END{print "commit;"}' words.txt |time mysql test
